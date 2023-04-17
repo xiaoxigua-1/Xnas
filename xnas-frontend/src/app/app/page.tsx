dee4a55ad76afebda6f0116d656bab0e0ee17958
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2023-04-17 12:34:18 +0000

    add windows stack

--- xnas-frontend/src/app/app/page.tsx
+++ xnas-frontend/src/app/app/page.tsx
@@ -1,54 +1,61 @@
 'use client';
 
 import { Box } from "@chakra-ui/react";
-import Window, { WindowType } from "./window";
+import Window, { WindowState, WindowType } from "./window";
 import React, { useState } from "react";
 
 export default function App() {
   const [windows, setWindows] = useState<WindowType[]>([
     {
-      x: 0,
-      y: 0,
-      width: 100,
-      height: 100,
-      move: false
+      x: 20,
+      y: 20,
+      width: 300,
+      height: 300,
+      move: false,
+      state: WindowState.Normal,
+      title: "123"
     },
     {
-      x: 200,
-      y: 400,
-      width: 100,
-      height: 100,
-      move: false
+      x: 20,
+      y: 20,
+      width: 300,
+      height: 300,
+      move: false,
+      state: WindowState.Normal,
+      title: "321"
     },
     {
-      x: 500,
-      y: 800,
-      width: 100,
-      height: 100,
-      move: false
+      x: 20,
+      y: 20,
+      width: 300,
+      height: 300,
+      move: false,
+      state: WindowState.Normal,
+      title: "321"
     },
   ]);
+  const [windowIndex, setWindowIndex] = useState<number[]>(windows.map((_, i) => i));
 
   const moveStart = (event: React.MouseEvent<HTMLDivElement>) => (index: number) => {
     const windowsClone = [...windows];
-    const item = windows[index];
+    const windowIndexClone = [...windowIndex];
+    const item = windowsClone[index];
 
     item.move = true;
     item.clickX = event.clientX - item.x;
 
-    windowsClone.splice(index, 1);
-    windowsClone.splice(0, 0, item);
-    setWindows(windowsClone);
+    windowIndexClone.splice(windowIndex.indexOf(index), 1);
+    windowIndexClone.splice(0, 0, index);
 
-    console.log("move start");
+    setWindowIndex(windowIndexClone);
+    setWindows(windowsClone);
   };
 
   const move = (event: React.MouseEvent<HTMLDivElement>) => {
     const windowsClone = [...windows];
   
     windowsClone.map(w => {
-      if (w.move) {
-        console.log(w.x, event.clientX);
+      if (w.move && w.state == WindowState.Normal) {
         w.x = event.clientX - (w.clickX ?? (w.width / 2));
         w.y = event.clientY - 10;
       }
@@ -60,7 +67,7 @@ export default function App() {
   };
 
   const moveEnd = () => {
-    let windowsClone = [...windows];
+    const windowsClone = [...windows];
     
     windowsClone.map(w => {
       w.move = false;
@@ -72,17 +79,33 @@ export default function App() {
 
   return (
     <Box 
-      className="w-full h-full bg-[url(/app/background)] bg-cover bg-center bg-no-repeat"
+      className="w-full h-full bg-[url(/app/background)] bg-cover bg-center bg-no-repeat overflow-hidden"
       onMouseMove={move}
       onMouseUp={moveEnd} 
     >
       {windows.map((window, index) => (
         <Window
-          key={index}
-          zIndex={windows.length - index}
           {...window}
+          key={index}
+          zIndex={windows.length - windowIndex[index]}
           onMouseDown={(event) => {
             moveStart(event)(index);
+          }}
+          onClose={() => {
+            const windowsClone = [...windows];
+            windowsClone.splice(index, 1);
+            setWindows(windowsClone);
+          }}
+          onMaximize={() => {
+            const windowsClone = [...windows];
+            const state = windowsClone[index].state;
+
+            windowsClone[index].state = state == WindowState.Normal ? WindowState.Maximize : WindowState.Normal;
+
+            setWindows(windowsClone);
+          }}
+          onMinimize={() => {
+            const windowsClone = [...windows];
           }}
         /> 
       ))} 
--- xnas-frontend/src/app/app/window.tsx
+++ xnas-frontend/src/app/app/window.tsx
@@ -1,29 +1,71 @@
-import { Box } from "@chakra-ui/react";
+import { Box, HStack } from "@chakra-ui/react";
+import { AiOutlineClose, AiOutlineBorder, AiOutlineLine } from "react-icons/ai";
 import { NextPage } from "next";
+
+export enum WindowState {
+  Normal,
+  Maximize,
+  Mininize
+}
 
 export interface WindowType extends React.HTMLAttributes<HTMLDivElement> {
   width: number;
   height: number;
   x: number;
   y: number;
+  title: string;
   move: boolean;
   clickX?: number;
+  state: WindowState
 }
 
-const Window: NextPage<WindowType & { zIndex: number }> = ({ width, height, x, y, move, zIndex, ...props }) => {
+export interface WindowFun {
+  onClose: () => void;
+  onMinimize: () => void;
+  onMaximize: () => void;
+}
+
+const Window: NextPage<WindowType & { zIndex: number } & WindowFun> = ({
+  width,
+  height,
+  x,
+  y,
+  move,
+  state,
+  title,
+  zIndex,
+  onMouseDown,
+  onClose,
+  onMaximize,
+  onMinimize,
+  ...props
+}) => {
+  const top = state == WindowState.Normal ? y : state == WindowState.Maximize ? 0 : 0;
+  const left = state == WindowState.Normal ? x : state == WindowState.Maximize ? 0 : 0;
+
   return (
     <Box 
-      className="absolute bg-white shadow-2xl"
+      className={`absolute bg-white shadow-2xl rounded-md ${move ? "" : "transition-maximize"}`}
       style={{
-        width,
-        height,
-        top: y,
-        left: x,
+        width: state == WindowState.Normal ? width : window.innerWidth,
+        height: state == WindowState.Normal ? height : window.innerHeight,
+        top,
+        left,
         zIndex
       }}
       {...props}
     >
-
+      <HStack 
+        justify="end"
+        spacing={3}
+        className="border-b-gray-400 border-b-[1px] p-1"
+        onMouseDown={onMouseDown}
+      >
+        <AiOutlineLine />
+        <AiOutlineBorder onClick={onMaximize} />
+        <AiOutlineClose onClick={onClose} />
+      </HStack>
+      <Box>{title}</Box>
     </Box>
   );
 };
